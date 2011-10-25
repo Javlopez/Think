b213cdf0783f47db251dce26b07f0b0f24da2b34
Javier Lopez Lopez <sjavierlopez@gmail.com>
2011-10-25 14:52:48 -0500

    Created index.php and rename Framework

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-#Thinf Framework
+#Think Framework
 
 Este framework, no pretendo competir contra otros, tampoco usarlo, es solo
 para mejorar mis conocimientos, estara en PHP5.3+ usando caracteristicas avanzadas
 
-Y como su nombre lo indica THINF "This is not a framework" (Este no es un framework) donde
-insisto que solo lo estoy haciendo para jugar.
+###Aunque cualquiera puede usarlo sin compromiso, si a alguien le gusta con gusto lo ire mejorando
+
--- index.php
+++ index.php
@@ -1,10 +1,40 @@
 <?php
+/**
+  * Think PHP Framework 
+  *
+  * Think PHP Framework is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * Think PHP Framework is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with Think PHP Framework.  If not, see <http://www.gnu.org/licenses/>.
+  *
+  */
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', TRUE);
+ini_set('error_reporting', E_ALL);
+
+
+define("__PATH__",__DIR__.DIRECTORY_SEPARATOR);
 
 
 
-require "system/core/thinf.php";
+$system      = 'system';
+$application = 'application';
+$modules     = $application.DIRECTORY_SEPARATOR."modules";
 
-echo Application::instance()->execute();
- 
 
-?> 
+define("__APP__",    realpath(__PATH__.$application).DIRECTORY_SEPARATOR);
+define("__SYSTEM__", realpath(__PATH__.$system).DIRECTORY_SEPARATOR);
+define("__MODULES__",realpath(__PATH__.$modules).DIRECTORY_SEPARATOR);
+
+
+
+require __SYSTEM__."core.php";
